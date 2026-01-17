e0ba7ff9af8f7fda2f54b50af4b32bafdf547165
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-17 17:47:04 +0100

    add navbar

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -8,6 +8,20 @@ function MainHeader() {
         <figure>
             <img src="/dc-logo.png" alt="Logo DC"></img> {/* con / entro in public */}
         </figure>
+        <nav>
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
     </header>
   )
 }
