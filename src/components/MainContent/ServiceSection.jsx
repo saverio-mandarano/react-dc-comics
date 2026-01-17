9783c70348b67469ba3582d9589eeaa34d05f518
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-17 20:44:46 +0100

    import della prima immagine in service section e add struttura base

--- src/components/MainContent/ServiceSection.jsx
+++ src/components/MainContent/ServiceSection.jsx
@@ -1,10 +1,58 @@
-export default function ServiceSection() {
+import buycomicsdigitalcomics from '../../assets/img/buy-comics-digital-comics.png';
 
+export default function ServiceSection() {
   return (
-    <section className='service'>
-      <div className='container'>
-        <p>&rarr; Serivces goes here &larr;</p>
+    <section className="service">
+      <div className="container">
+        <ul>
+          
+          <li>
+            <figure className="flex">
+              <a href="#">
+                <img src={buycomicsdigitalcomics} alt="" />
+              </a>
+              <figcaption>DIGITAL COMICS</figcaption>
+            </figure>
+          </li>
+
+          <li>
+            <figure>
+              <a href="#">
+                <img src="" alt="" />
+              </a>
+              <figcaption>DC MERCHANDISE</figcaption>
+            </figure>
+          </li>
+
+          <li>
+            <figure>
+              <a href="#">
+                <img src="" alt="" />
+              </a>
+              <figcaption>SUBSCRIPTION</figcaption>
+            </figure>
+          </li>
+
+          <li>
+            <figure>
+              <a href="#">
+                <img src="" alt="" />
+              </a>
+              <figcaption>COMICS SHOP LOCATOR</figcaption>
+            </figure>
+          </li>
+
+          <li>
+            <figure>
+              <a href="#">
+                <img src="" alt="" />
+              </a>
+              <figcaption>DC POWER VISA</figcaption>
+            </figure>
+          </li>
+
+        </ul>
       </div>
     </section>
-  )
+  );
 }
