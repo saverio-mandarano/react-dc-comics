a08122f49110883e5b49cf4b181fb979fd855dfe
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-17 15:57:00 +0100

    creo cartella components e creo il primo componente

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,25 @@
+function MainHeader() {
+
+  return (
+      <h2>Header</h2>
+  )
+}
+
+export default MainHeader
+
+//oppure
+// export default MainHeader() {
+//   return (
+//       <h1>Hello world</h1>
+//   )
+// }
+
+
+//oppure
+// const MainHeader = () => {
+//     return (
+//       <h2>Header</h2>
+//   )
+// }
+
+// export default MainHeader
