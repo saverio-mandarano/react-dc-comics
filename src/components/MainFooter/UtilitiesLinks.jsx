b564de07ca7353736a61db6e13d9faee1db28394
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-18 00:52:57 +0100

    add prima lista ma in orizzontale

--- src/components/MainFooter/UtilitiesLinks.jsx
+++ src/components/MainFooter/UtilitiesLinks.jsx
@@ -1,9 +1,26 @@
+import DcLogo from '../../assets/img/dc-logo-bg.png';
+
 export default function UtilitiesLinks () {
 
   return (
     <section className='utilities-links'>
       <div className='container'>
-        <h2>&rarr; UtilitiesLinks goes here &larr;</h2>
+        <div className="links-list">
+            <ul>
+                <li>DC COMICS</li>
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+            </ul>
+            
+        </div>
+        <div className="dc-img">
+            <img src={DcLogo} alt="" />
+        </div>
       </div>
     </section>
   )
