ca8d178bf1eb3e989f3961cfa0d8a382fc04e268
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-19 17:23:29 +0100

    add footer-icons

--- src/components/MainFooter/ContactUsLinks.jsx
+++ src/components/MainFooter/ContactUsLinks.jsx
@@ -1,3 +1,9 @@
+import Facebook from '../../assets/img/footer-facebook.png';
+import Twitter from '../../assets/img/footer-twitter.png';
+import Youtube from '../../assets/img/footer-youtube.png';
+import Pinterest from '../../assets/img/footer-pinterest.png';
+import Periscope from '../../assets/img/footer-periscope.png';
+
 export default function ContactUsLinks () {
 
   return (
@@ -6,7 +12,16 @@ export default function ContactUsLinks () {
         <div className="btn-sign-up">
           <button>SIGN-UP NOW</button>
         </div>
-        <div className="follow-us-icons"></div>        
+        <div className="follow-us-icons">
+          <ul>
+              <li><a href="">FOLLOW US</a></li>
+              <li><img src={Pinterest} alt="facebook icon" /></li>
+              <li><img src={Twitter} alt="" /></li>
+              <li><img src={Youtube} alt="" /></li>
+              <li><img src={Pinterest} alt="" /></li>
+              <li><img src={Periscope} alt="" /></li>
+            </ul>  
+        </div>        
       </div>
     </section>
   )
