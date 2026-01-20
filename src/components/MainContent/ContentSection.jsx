733f53b223a3d96c4979704aed3de999d9a577ac
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-20 01:34:07 +0100

    add cards img renderizzate

--- src/components/MainContent/ContentSection.jsx
+++ src/components/MainContent/ContentSection.jsx
@@ -4,29 +4,23 @@ export default function ContentSection() {
   // funzione di rendering prodotti
   const renderProdottiCard = () => {
     return comics.map((comic) => (
-      <div className="prod-card" key={comic.id}>
-        <img src={comic.thumb} alt={comic.description} />
-        <h3>{comic.series}</h3>
+      <div className= 'card' key={comic.id}>
+        <img src={comic.thumb}  />
+        <h5>{comic.series}</h5>
       </div>
     ))
-  }
-
+  } 
+// alt={comic.description}
   return (
-      <section className='content'>
-        <div className='container'>
-                  <section id="products">
-            <h2>Lista prodotti</h2>
-
-            <div className="container-custom card-container">
-
-                {renderProdottiCard()}
-
-
-            </div>
-
+    <section className='content'>
+      <div className='container'>
+        <section id="products">
+          <div className="card-container">
+            {renderProdottiCard()}
+          </div>
         </section>
-        </div>
-      </section>
+      </div>
+    </section>
   )
 }
 
