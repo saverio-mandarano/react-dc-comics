c86a4286edbda3bca5124e3532b972df2dfe9129
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-21 17:38:17 +0100

    aggiungere link nelle liste dinamicamente prendendo i dati dall'array creato

--- src/components/MainFooter/UtilitiesLinks.jsx
+++ src/components/MainFooter/UtilitiesLinks.jsx
@@ -1,56 +1,87 @@
-import DcLogo from '../../assets/img/dc-logo-bg.png';
+import utilitiesLinks from '../Data/footer-links'
 
-export default function UtilitiesLinks () {
+export default function UtilitiesLinks() {
+
+  // funzione di rendering
+  const renderUtilitiesLinks = () => {
+    return utilitiesLinks.map(section => (
+      <div className="links-column" key={section.category}>
+        <ul>
+          <li>
+            <h3>{section.category.toUpperCase()}</h3>
+          </li>
+
+          {section.links.map(link => (
+            <li key={link.id}>
+              <a href={link.url}>{link.text}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ));
+  };
 
   return (
-    <section className='utilities-links'>
-      <div className='container'>
+    <section className="utilities-links">
+      <div className="container">
         <div className="links-list">
-          <div className="links-column">
-            <ul>
-              <li><h3>DC COMICS</h3></li>
-              <li><a href="">Characters</a></li>
-              <li><a href="">Comics</a></li>
-              <li><a href="">Movies</a></li>
-              <li><a href="">TV</a></li>
-              <li><a href="">Games</a></li>
-              <li><a href="">Videos</a></li>
-              <li><a href="">News</a></li>
-            </ul>
-            <ul>
-              <li><h3>SHOP</h3></li>
-              <li><a href="">Shop DC</a></li>
-              <li><a href="">Shop DC Collectibles</a></li>
-            </ul>
-          </div>
-          <div className="links-column">
-            <ul>
-              <li><h3>DC</h3></li>
-              <li><a href="">Terms of use</a></li>
-              <li><a href="">{`Privacy policy (New)`}</a></li>
-              <li><a href="">Ad Choices</a></li>
-              <li><a href="">Advertising</a></li>
-              <li><a href="">Jobs</a></li>
-              <li><a href="">Subscriptions</a></li>
-              <li><a href="">Talent Workshops</a></li>
-              <li><a href="">CPSC Certificates</a></li>
-              <li><a href="">Ratings</a></li>
-              <li><a href="">Shop Help</a></li>
-              <li><a href="">Contact Us</a></li>
-            </ul>
-          </div>
-          <div className="links-column">
-            <ul>
-              <li><h3>SITES</h3></li>
-              <li><a href="">DC</a></li>
-              <li><a href="">MAD Magazine</a></li>
-              <li><a href="">DC Kids</a></li>
-              <li><a href="">DC Universe</a></li>
-              <li><a href="">DC Power Visa</a></li>
-            </ul>       
-            </div>
-          </div>
+          {renderUtilitiesLinks()}
+        </div>
       </div>
     </section>
-  )
+  );
 }
+
+  
+  
+  // return (
+  //   <section className='utilities-links'>
+  //     <div className='container'>
+  //       <div className="links-list">
+  //         <div className="links-column">
+  //           <ul>
+  //             <li><h3>DC COMICS</h3></li>
+  //             <li><a href="">Characters</a></li>
+  //             <li><a href="">Comics</a></li>
+  //             <li><a href="">Movies</a></li>
+  //             <li><a href="">TV</a></li>
+  //             <li><a href="">Games</a></li>
+  //             <li><a href="">Videos</a></li>
+  //             <li><a href="">News</a></li>
+  //           </ul>
+  //           <ul>
+  //             <li><h3>SHOP</h3></li>
+  //             <li><a href="">Shop DC</a></li>
+  //             <li><a href="">Shop DC Collectibles</a></li>
+  //           </ul>
+  //         </div>
+  //         <div className="links-column">
+  //           <ul>
+  //             <li><h3>DC</h3></li>
+  //             <li><a href="">Terms of use</a></li>
+  //             <li><a href="">{`Privacy policy (New)`}</a></li>
+  //             <li><a href="">Ad Choices</a></li>
+  //             <li><a href="">Advertising</a></li>
+  //             <li><a href="">Jobs</a></li>
+  //             <li><a href="">Subscriptions</a></li>
+  //             <li><a href="">Talent Workshops</a></li>
+  //             <li><a href="">CPSC Certificates</a></li>
+  //             <li><a href="">Ratings</a></li>
+  //             <li><a href="">Shop Help</a></li>
+  //             <li><a href="">Contact Us</a></li>
+  //           </ul>
+  //         </div>
+  //         <div className="links-column">
+  //           <ul>
+  //             <li><h3>SITES</h3></li>
+  //             <li><a href="">DC</a></li>
+  //             <li><a href="">MAD Magazine</a></li>
+  //             <li><a href="">DC Kids</a></li>
+  //             <li><a href="">DC Universe</a></li>
+  //             <li><a href="">DC Power Visa</a></li>
+  //           </ul>       
+  //           </div>
+  //         </div>
+  //     </div>
+  //   </section>
+  // )
